3e3683a2718697940d3d3d0a1baf74e03c5d6735
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2022-05-30 15:49:03 +0200

    first commit

---
+++ src/connect.py
@@ -0,0 +1,14 @@
+import network
+
+WiFi_SSID = "***********"
+WiFi_PASS = "***********"
+
+def do_connect():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect(WiFi_SSID, WiFi_PASS)
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
